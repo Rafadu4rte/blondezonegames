f1ac0a7bcdc6e2d4b4bc1aaf79c55c85c7070ea8
VINICIUS LOPES DA SILVA <0200832311013>
2024-10-29 11:12:25 -0300

    dawdawda

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-document.getElementById('toggleTheme').addEventListener('click', function() {
+const toggle = () => {
+    const theme = document.querySelector('button')
     const body = document.body;
     body.classList.toggle('light-mode');
     body.classList.toggle('dark-mode');
-});
+    // header.classList.toggle('light-mode');
+    // header.classList.toggle('dark-mode');
+    document.querySelector('.explore').classList.toggle('text-black')
+    document.querySelector('.icon-mode').classList.toggle('hidden')
+    theme.classList.toggle('hover:bg-zinc-200')
 
-// Animação ao passar o mouse nos itens
-const gameCards = document.querySelectorAll('.game-card');
-gameCards.forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.classList.add('shadow-2xl');
-    });
-    card.addEventListener('mouseleave', () => {
-        card.classList.remove('shadow-2xl');
-    });
-});
+    if(document.querySelector('.icon-mode').classList.contains('hidden')){
+        document.querySelector('.moon').classList.add('flex')
+    }
+};
+
+// const gameCards = document.querySelectorAll('.game-card');
+// gameCards.forEach(card => {
+//     card.addEventListener('mouseenter', () => {
+//         card.classList.add('shadow-2xl');
+//     });
+//     card.addEventListener('mouseleave', () => {
+//         card.classList.remove('shadow-2xl');
+//     });
+// });
